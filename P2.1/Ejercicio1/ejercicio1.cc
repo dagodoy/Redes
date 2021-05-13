670d595bd78d6d097dd060ff30db7e1763e74296
root <root@localhost.localdomain>
2021-05-13 21:39:45 +0200

    Primer commit

---
+++ P2.1/Ejercicio1/ejercicio1.cc
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+
+#include <iostream>
+
+
+int main (int argc, char** argv)
+{
+	struct addrinfo hints;
+	struct addrinfo * res;
+	
+	memset ((void*) &hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+
+	int rc = getaddrinfo(argv[1], NULL, &hints, &res);
+	if (rc != 0)
+	{
+		std::cerr << "Error: " << gai_strerror(rc) << std::endl;
+		return -1;
+	}
+
+	for (struct addrinfo * i = res; i != NULL; i = i->ai_next)
+	{
+		char host[NI_MAXHOST];
+		char serv[NI_MAXSERV];
+	  	
+		getnameinfo(i->ai_addr, i->ai_addrlen, host, NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST);
+		std::cout << host << " " << i->ai_family << "	" << i->ai_socktype << std::endl;
+	}
+
+	freeaddrinfo(res);
+	return 0;
+}
